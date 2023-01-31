edfa7a3adaebea5645a6fe7afc3758bb3670f631
Sergey Evtushenko <evtushru208@gmail.com>
2023-01-31 22:52:43 +0200

    добаавил папку content.js

--- js/mvc/controller.js
+++ js/mvc/controller.js
@@ -1,20 +1,14 @@
 
-import {liArr,burger,body} from "./view/header.js";
-
-
-
-
+import {liArr,burger} from "./view/header.js";
 
 liArr.forEach((el)=>{
    el.elem.addEventListener('click',(e)=>{
       e.preventDefault();
       const elClass='.'+e.target.getAttribute('data-goto');
       const elem=document.querySelector(elClass);
-      console.log(elem);
       el.scrollToElement(elem);
    });
 });
-
 
 burger.elem.addEventListener('click',(e)=>{
    e.preventDefault();
